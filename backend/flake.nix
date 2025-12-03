295e10122ca5c863d4e400607542cc15de1f447d
Daci1 <dacian.barbu28@gmail.com>
2025-12-03 15:43:27 +0200

    Adds back nix related stuff
    Gotta love updating your linux system, right? RIGHT?

---
+++ backend/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        libgcc
+      ];
+      env = {
+        CGO_ENABLED = "0";
+      };
+    };
+  };
+}
